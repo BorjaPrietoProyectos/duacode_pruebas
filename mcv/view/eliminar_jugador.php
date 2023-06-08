c19db018718daed23f11d84336b7324ee4d8570d
BorjaPrietoProyectos <104348206+BorjaPrietoProyectos@users.noreply.github.com>
2023-06-08 12:07:52 +0200

    eliminar jugador

--- mcv/view/eliminar_jugador.php
+++ mcv/view/eliminar_jugador.php
@@ -1,15 +1,21 @@
 <?php
+require_once 'mcv\controller\team_controller.php';
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $jugadorId = $_POST['jugador_id'];
 
-    // Realiza la eliminación del jugador
+
+    $equipoController = new EquipoController();
+
+
     $resultado = $equipoController->eliminarJugador($jugadorId);
 
     if ($resultado) {
-        // Redirecciona a la página de mostrar equipo
-        header('Location: mostrar_equipo.php?equipo_id=' . $equipoId);
+
+        header('Location: mcv\view\mostrar_equipo.php?equipo_id=' . $equipoId);
         exit;
     } else {
         echo "Error al eliminar el jugador.";
     }
 }
+?>
